5bd9f99bd9236e07e3eed15db79d1b37e1a5f12e
Bryan Ross <git@rossipedia.com>
2017-09-17 12:43:30 -0600

    position: sticky is just too brittle when we don't control the whole page

--- settings-panel/src/components/SettingsList/index.tsx
+++ settings-panel/src/components/SettingsList/index.tsx
@@ -245,9 +245,6 @@ const SettingGroup = styled.p`
   @media (max-width: ${smallWidth}px) {
     margin-top: 1em;
   }
-
-  position: sticky;
-  top: 0;
 `;
 
 const listItemColors = {
@@ -290,7 +287,7 @@ const ListItemContainer: React.StatelessComponent<
   box-shadow: ${p =>
     p.selected ? '0px 0px 2px 1px hsla(194, 60%, 60%, 1)' : 'none'};
   position: ${p => (p.selected ? 'relative' : 'static')};
-  z-index: ${p => (p.selected ? 50 : 0)};
+  z-index: 1;
 
   > :first-child {
     background-color: ${p =>
