b97a168a913b1f7af2d1ae207b139d9410bd5e57
Bryan J. Ross <bryan@rossipedia.com>
2018-06-15 14:24:46 -0600

    csproj upgrade to sdk project format

--- NFig.UI/NFigStoreExtensions.cs
+++ NFig.UI/NFigStoreExtensions.cs
@@ -1,103 +1,111 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Jil;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace NFig.UI
 {
-  public static class NFigStoreExtensions
-  {
-    static readonly Options s_jsonOptions = Options.ISO8601ExcludeNullsIncludeInheritedUtcCamelCase;
-
-
-    public static string GetSettingsJson<TSettings, TTier, TDataCenter>(
-      this NFigStore<TSettings, TTier, TDataCenter> store,
-      string applicationName,
-      TTier currentTier,
-      TDataCenter currentDataCenter,
-      IList<TDataCenter> availableDataCenters)
-      where TTier : struct
-      where TDataCenter : struct
-      where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
+    public static class NFigStoreExtensions
     {
-      if (store == null) throw new ArgumentNullException(nameof(store));
+        static readonly JsonSerializerSettings s_jsonSettings = new JsonSerializerSettings
+        {
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            NullValueHandling = NullValueHandling.Ignore,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        };
 
-      var infos = store.GetAllSettingInfos(applicationName);
-      return JSON.Serialize(
-        new SettingsJsonModel<TTier, TDataCenter>(
-          currentTier,
-          currentDataCenter,
-          infos,
-          availableDataCenters),
-        s_jsonOptions);
+        public static string GetSettingsJson<TSettings, TTier, TDataCenter>(
+          this NFigStore<TSettings, TTier, TDataCenter> store,
+          string applicationName,
+          TTier currentTier,
+          TDataCenter currentDataCenter,
+          IList<TDataCenter> availableDataCenters)
+          where TTier : struct
+          where TDataCenter : struct
+          where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
+        {
+            if (store == null) throw new ArgumentNullException(nameof(store));
+
+            var infos = store.GetAllSettingInfos(applicationName);
+            var model = new SettingsJsonModel<TTier, TDataCenter>(
+                currentTier,
+                currentDataCenter,
+                infos,
+                availableDataCenters
+            );
+
+            return JsonConvert.SerializeObject(model, s_jsonSettings);
+        }
+
+
+        public static async Task<string> GetSettingsJsonAsync
+          <TSettings, TTier, TDataCenter>(
+            this NFigAsyncStore<TSettings, TTier, TDataCenter> store,
+            string applicationName,
+            TTier currentTier,
+            TDataCenter currentDataCenter,
+            IList<TDataCenter> availableDataCenters)
+          where TTier : struct
+          where TDataCenter : struct
+          where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
+        {
+            if (store == null) throw new ArgumentNullException(nameof(store));
+
+            var infos = await store.GetAllSettingInfosAsync(applicationName);
+            var model = new SettingsJsonModel<TTier, TDataCenter>(
+                currentTier,
+                currentDataCenter,
+                infos,
+                availableDataCenters
+            );
+
+            return JsonConvert.SerializeObject(model, s_jsonSettings);
+        }
+
+
+        public static string GetSettingJson<TSettings, TTier, TDataCenter>(
+          this NFigStore<TSettings, TTier, TDataCenter> store,
+          string applicationName,
+          string settingName,
+          TTier tier,
+          TDataCenter dataCenter,
+          IList<TDataCenter> availableDataCenters)
+          where TTier : struct
+          where TDataCenter : struct
+          where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
+        {
+            var model = new SettingsJsonModel<TTier, TDataCenter>.Setting(
+                tier,
+                dataCenter,
+                store.GetSettingInfo(applicationName, settingName),
+                availableDataCenters
+            );
+            return JsonConvert.SerializeObject(model, s_jsonSettings);
+        }
+
+
+        public static async Task<string> GetSettingJsonAsync
+          <TSettings, TTier, TDataCenter>(
+            this NFigAsyncStore<TSettings, TTier, TDataCenter> store,
+            string applicationName,
+            string settingName,
+            TTier tier,
+            TDataCenter dataCenter,
+            IList<TDataCenter> availableDataCenters)
+          where TTier : struct
+          where TDataCenter : struct
+          where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
+        {
+            var model = new SettingsJsonModel<TTier, TDataCenter>.Setting(
+                tier,
+                dataCenter,
+                await store.GetSettingInfoAsync(applicationName, settingName),
+                availableDataCenters
+            );
+
+            return JsonConvert.SerializeObject(model, s_jsonSettings);
+        }
     }
-
-
-    public static async Task<string> GetSettingsJsonAsync
-      <TSettings, TTier, TDataCenter>(
-        this NFigAsyncStore<TSettings, TTier, TDataCenter> store,
-        string applicationName,
-        TTier currentTier,
-        TDataCenter currentDataCenter,
-        IList<TDataCenter> availableDataCenters)
-      where TTier : struct
-      where TDataCenter : struct
-      where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
-    {
-      if (store == null) throw new ArgumentNullException(nameof(store));
-
-      var infos = await store.GetAllSettingInfosAsync(applicationName);
-
-      return JSON.Serialize(
-        new SettingsJsonModel<TTier, TDataCenter>(
-          currentTier,
-          currentDataCenter,
-          infos,
-          availableDataCenters),
-        s_jsonOptions);
-    }
-
-
-    public static string GetSettingJson<TSettings, TTier, TDataCenter>(
-      this NFigStore<TSettings, TTier, TDataCenter> store,
-      string applicationName,
-      string settingName,
-      TTier tier,
-      TDataCenter dataCenter,
-      IList<TDataCenter> availableDataCenters)
-      where TTier : struct
-      where TDataCenter : struct
-      where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
-    {
-      return JSON.Serialize(
-        new SettingsJsonModel<TTier, TDataCenter>.Setting(
-          tier,
-          dataCenter,
-          store.GetSettingInfo(applicationName, settingName),
-          availableDataCenters),
-        s_jsonOptions);
-    }
-
-
-    public static async Task<string> GetSettingJsonAsync
-      <TSettings, TTier, TDataCenter>(
-        this NFigAsyncStore<TSettings, TTier, TDataCenter> store,
-        string applicationName,
-        string settingName,
-        TTier tier,
-        TDataCenter dataCenter,
-        IList<TDataCenter> availableDataCenters)
-      where TTier : struct
-      where TDataCenter : struct
-      where TSettings : class, INFigSettings<TTier, TDataCenter>, new()
-    {
-      return JSON.Serialize(
-        new SettingsJsonModel<TTier, TDataCenter>.Setting(
-          tier,
-          dataCenter,
-          await store.GetSettingInfoAsync(applicationName, settingName),
-          availableDataCenters),
-        s_jsonOptions);
-    }
-  }
 }
